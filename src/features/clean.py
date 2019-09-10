fa393c082204141d5159c154ad6518caeee59d65
Bartłomiej Poniecki-Klotz <b.poniecki.klotz@gmail.com>
2019-09-10 22:35:39 +0200

    full cleanup

--- src/features/clean.py
+++
@@ -1,6 +0,0 @@
-from imblearn.over_sampling import SMOTE
-
-
-def oversampling(X, y):
-    os = SMOTE(random_state=42, k_neighbors=4)
-    return os.fit_resample(X, y)
--- src/features/feature.py
+++
@@ -1,6 +0,0 @@
-# TODO add features engineering
-def feature_engineering(data):
-    data['TimesBeforeLast'] = data.apply(lambda x: x['Times'] - x['Recency'], axis=1)
-    data['FrequencyBeforeLast'] = data.apply(lambda x: (x['Times'] - x['Recency']) / x['Frequency'], axis=1)
-    return data
-
